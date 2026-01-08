c140c4e16b6246215d8fdd0d7a6b2e64af10ed02
Manishkumarsingh6239 <manishkumarsingh6239@gmail.com>
2026-01-08 23:29:54 +0530

    .

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from '../components/navbar'
 
 const DashboardPage = () => {
   return (
     <div>
-      Dashboard Page
+      <Navbar/>
     </div>
   )
 }
