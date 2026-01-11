06d026c7eeb927262268cdab87229bcfa87dd435
Manishkumarsingh6239 <manishkumarsingh6239@gmail.com>
2026-01-11 21:53:21 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,22 +3,24 @@ import { useState } from 'react'
 import { Toaster } from 'react-hot-toast';
 import { Routes, Route, Navigate } from 'react-router';
 import HomePage from './pages/HomePage.jsx';
-import ProblemPage from './pages/ProblemPage.jsx';
+import ProblemsPage from './pages/ProblemsPage.jsx';
 import DashboardPage from './pages/DashboardPage.jsx';
+import ProblemPage from './pages/ProblemPage.jsx';
 
 
 function App() {
-  const {isSignedIn,isLoaded} = useUser()
-  if(!isLoaded) return null;
+  const { isSignedIn, isLoaded } = useUser()
+  if (!isLoaded) return null;
   console.log(isSignedIn)
   return (
     <>
       <Routes>
         <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
         <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
-        <Route path="/problems" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
+        <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+        <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
       </Routes>
-      <Toaster position="top-center" reverseOrder={false} />
+      <Toaster toastOptions={{duration: 3000}} />
     </>
   )
 }
