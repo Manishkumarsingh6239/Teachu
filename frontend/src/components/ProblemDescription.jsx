3b16c31ff13ffe05a9de50f96bf60cce77982fa2
Manishkumarsingh6239 <manishkumarsingh6239@gmail.com>
2026-01-21 23:31:38 +0530

    Started with problemDescription page

--- frontend/src/components/ProblemDescription.jsx
+++ frontend/src/components/ProblemDescription.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function ProblemDescription() {
   return (
-    <div>
-      ProblemDescription
+    <div className='h-full overflow-y-auto bg-base-200'>
+      <div className='p-6 bg-base-100 border-b border-base-100'>
+         
+      </div>
     </div>
   )
 }
