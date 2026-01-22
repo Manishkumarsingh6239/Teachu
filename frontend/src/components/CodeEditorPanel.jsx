9afda492d5112691a50fbf680ca50c1b3d2d6e6b
Manishkumarsingh6239 <manishkumarsingh6239@gmail.com>
2026-01-22 23:09:57 +0530

    Code Editor added

--- frontend/src/components/CodeEditorPanel.jsx
+++ frontend/src/components/CodeEditorPanel.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import Editor from '@monaco-editor/react'
+import {Loader2Icon, PlayIcon} from 'lucide-react'
 import { LANGUAGE_CONFIG } from '../data/problems'
 function CodeEditorPanel({
   selectedLanguage,
@@ -10,7 +11,7 @@ function CodeEditorPanel({
 }) {
   return (
     <div className='h-full bg-base-300 flex flex-col'>
-      <div className="flex items-center justify-baseline px-4 py-3 bg-base-100 border-t border-base-300">
+      <div className="flex items-center justify-between px-4 py-3 bg-base-100 border-t border-base-300 gap-2">
         <div className="flex items-center gap-3">
           <img 
           src={LANGUAGE_CONFIG[selectedLanguage].icon}
@@ -26,6 +27,38 @@ function CodeEditorPanel({
             ))}
           </select>
         </div>
+        <button className='btn btn-sm btn-primary gap-2' onClick={onRunCode}>
+            {isRunning ? (
+              <>
+                <Loader2Icon className="size-4 animate-spin" />
+                Running...
+              </>
+            ):
+            (
+              <>
+                <PlayIcon className="size-4" />
+                Run Code
+              </>
+            )
+            }
+        </button>
+      </div>
+
+      <div className="flex-1">
+        <Editor 
+        height="100%"
+        language={LANGUAGE_CONFIG[selectedLanguage].monacoLang}
+        value={code}
+        onChange={onCodeChange}
+        theme='vs-dark'
+        options={{
+          fontSize:14,
+          lineNumbers:"on",
+          scrollBeyondLastLine:false,
+          automaticLayout:true,
+          minimap:{enabled:false}
+        }}
+        />
       </div>
     </div>
   )
