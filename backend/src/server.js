6e7a38eddef2fd3f6fb7cff4c01d693d82865bfb
Manishkumarsingh6239 <manishkumarsingh6239@gmail.com>
2026-01-28 22:33:56 +0530

    .

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,59 +1,50 @@
 import express from "express";
 import path from "path";
 import cors from "cors";
+import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
+
 import { ENV } from "./lib/env.js";
 import { ConnectDB } from "./lib/db.js";
-import { serve } from "inngest/express";
 import { inngest, functions } from "./lib/inngest.js";
-import { clerkMiddleware } from "@clerk/express";
-import { protectRoute } from "./middleware/protectRoute.js";
+
 import chatRoutes from "./routes/chatRoute.js";
-import sessionRoute from "./routes/sessionRoute.js";
+import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
+
 const __dirname = path.resolve();
 
-// middlewares
+// middleware
 app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
-if (ENV.NODE_ENV !== "production") {
-  app.use(cors({
-    origin: ENV.CLIENT_URL,
-    credentials: true
-  }));
-}
-
-app.use(clerkMiddleware());
-
-// API routes
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
-app.use("/api/sessions", sessionRoute);
+app.use("/api/sessions", sessionRoutes);
 
-app.get("/protected-route", protectRoute, (req, res) => {
-  res.status(200).json({ msg: "API is working" });
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
 });
 
-// frontend
+// make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.join(__dirname, "../frontend/dist/index.html")
-    );
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-const StartServer = async () => {
+const startServer = async () => {
   try {
     await ConnectDB();
-    app.listen(ENV.PORT, () => {
-      console.log("Server running on port", ENV.PORT);
-    });
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
   } catch (error) {
-    console.error("💥 Error starting server", error);
+    console.error("💥 Error starting the server", error);
   }
 };
 
-StartServer();
+startServer();
